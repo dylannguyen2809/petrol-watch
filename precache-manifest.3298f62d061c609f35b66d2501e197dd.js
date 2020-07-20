a880b3e849f92081115af077247f8b5125b4593e
dylannguyen2809 <dylannguyen289@gmail.com>
2020-07-20 23:02:46 +1000

    Updates

---
+++ precache-manifest.3298f62d061c609f35b66d2501e197dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e967141320ef1ff74eededb0aa20d44b",
+    "url": "/petrol-watch/index.html"
+  },
+  {
+    "revision": "e504e6051567926f6bbb",
+    "url": "/petrol-watch/static/css/main.2b814a55.chunk.css"
+  },
+  {
+    "revision": "cf29dca9ee02477653a2",
+    "url": "/petrol-watch/static/js/2.a72976a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/petrol-watch/static/js/2.a72976a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e504e6051567926f6bbb",
+    "url": "/petrol-watch/static/js/main.3d9d7f32.chunk.js"
+  },
+  {
+    "revision": "340481722d9bf25f055e",
+    "url": "/petrol-watch/static/js/runtime-main.b93e59ea.js"
+  }
+]);
